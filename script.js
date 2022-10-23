787670ba184101b220ebabcf7650b8c4a2a5059b
yuukiabc <70953219+yuukiabc@users.noreply.github.com>
2022-10-23 22:17:46 +0900

    a
    a

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(function() {
-    $('#delete').click(function() {
-        $('#text').hide();
-    });
+$('.list_item').click(function() {
+    $('.header_item').text('メニュークリック処理が実行されました');
+    $('#photo').fadeIn(10000);
+    alert('チョッパー登場');
+});
+$('#delete').click(function() {
+    $('.header_item').text('画像消えます');
+    $('#photo').fadeOut(1000);
+});
+
 
 
 });
