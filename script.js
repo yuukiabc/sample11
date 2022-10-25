91a39cbd54a0ed367537e392830e7a2bdcc94c6d
yuukiabc <70953219+yuukiabc@users.noreply.github.com>
2022-10-25 21:15:30 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 $(function() {
+    $('.header_item').click(function(){
+        $('html, body').animate({ 
+          'scrollTop': 0 
+        }, 500);
+      });
 $('.list_item').click(function() {
-    $('.header_item').text('メニュークリック処理が実行されました');
+    $('.header_deputyitem').text('メニュークリック処理が実行されました');
     $('#photo').fadeIn(10000);
     alert('チョッパー登場');
     $('.short_message').fadeOut(10000);
 });
 $('#delete').click(function() {
-    $('.header_item').text('スローで画像消えます');
+    $('.header_deputyitem').text('スローで画像消えます');
     $('#photo').fadeOut(1000);
     $('.short_message').fadeIn();
 });
@@ -16,7 +21,7 @@ $('#delete').click(function() {
 //});
 
 $('.slider').slick({
-    arrows: false,
+    
     dots: true,
     autoplay: true
 });
